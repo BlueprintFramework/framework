aecfa5038d54bc8ce910a83b146aa4ee74ed16f1
purple <me@purple.lol>
2023-02-07 17:16:49 +0100

    (kigipux) Applied custom css changes.
    Now a tag shows instead of just a small text.

--- resources/views/admin/extensions/blueprint/index.blade.php
+++ resources/views/admin/extensions/blueprint/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('content-header')
     <img src="/assets/extensions/blueprint/logo.jpg" alt="logo" style="float:left;width:30px;height:30px;border-radius:3px;margin-right:5px;">
-    <h1>Blueprint<small>indev</small></h1>
+    <h1 ext-title>Blueprint<tag mg-left blue>indev</tag></h1>
     <ol class="breadcrumb">
         <li><a href="{{ route('admin.index') }}">Admin</a></li>
         <li><a href="{{ route('admin.extensions') }}">Extensions</a></li>
