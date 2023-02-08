e54d67d43aff8686eac75024a6e3300d745bd58f
purple <me@purple.lol>
2023-02-08 11:26:12 +0100

    Added a button row
    The admin page for Blueprint now has a placeholder button row with custom injected css.

--- resources/views/admin/extensions/blueprint/index.blade.php
+++ resources/views/admin/extensions/blueprint/index.blade.php
@@ -17,16 +17,16 @@
 @section('content')
     <div class="row">
         <div class="col-xs-6 col-sm-3 text-center">
-            <a href=""><button class="btn btn-transparent" style="width:100%;"><i class='bx bx-trash-alt'></i> placeholder </button></a>
+            <a href=""><button class="btn btn-gray" style="width:100%;"><i class='bx bx-link'></i> placeholder </button></a>
         </div>
         <div class="col-xs-6 col-sm-3 text-center">
-            <a href="https://pterodactyl.io"><button class="btn btn-clear" style="width:100%;"><i class="fa fa-fw fa-link"></i> placeholder </button></a>
+            <a href=""><button class="btn btn-gray" style="width:100%;"><i class='bx bx-link'></i> placeholder </button></a>
         </div>
         <div class="col-xs-6 col-sm-3 text-center">
-            <a href="https://github.com/pterodactyl/panel"><button class="btn btn-clear" style="width:100%;"><i class="fa fa-fw fa-support"></i> placeholder </button></a>
+            <a href=""><button class="btn btn-gray" style="width:100%;"><i class='bx bx-link'></i> placeholder </button></a>
         </div>
         <div class="col-xs-6 col-sm-3 text-center">
-            <a href=""><button class="btn btn-clear" style="width:100%;"><i class="fa fa-fw fa-money"></i> placeholder </button></a>
+            <a href=""><button class="btn btn-gray" style="width:100%;"><i class='bx bx-link'></i> placeholder </button></a>
         </div>
     </div>
     <p>placeholder</p>
