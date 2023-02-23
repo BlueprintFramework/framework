68366febe8f1a5c20da029c05b607d10c3dcd285
purple <me@purple.lol>
2023-02-23 21:00:46 +0100

    Fixed a small issue.

--- resources/views/admin/extensions.blade.php
+++ resources/views/admin/extensions.blade.php
@@ -14,7 +14,7 @@
 
 @section('content')
     <div class="col-xs-6 col-sm-3 text-center">
-        <a href="{{ route('admin.extensions.blueprint.index') }}"><button class="btn btn-gray btn-row"><img src="/assets/extensions/blueprint/logo.jpg" alt="logo" class="img-btn"> Blueprint <small>{{ $bp->version() }}</small></button></a>
+        <a href="{{ route('admin.extensions.blueprint.index') }}"><button class="btn btn-gray btn-row" style="width:100%;"><img src="/assets/extensions/blueprint/logo.jpg" alt="logo" class="img-btn"> Blueprint <small>{{ $bp->version() }}</small></button></a>
     </div>
     <!-- blueprint.replace -->
 @endsection
