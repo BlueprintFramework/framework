cc410b1e35ee31084d79a1473b532a4a55921d85
purple <hello@prpl.wtf>
2023-03-06 19:11:53 +0100

    Added more installation commands
    Got to prepare for the future.

--- blueprint.sh
+++ blueprint.sh
@@ -5,6 +5,9 @@ warn="\033[0;33m WARN \033[0m";
 info="\033[0;96m INFO \033[0m";
 task="\033[0;93m TASK \033[0m";
 
+cd /var/www/pterodactyl;
+mkdir -p .blueprint > /dev/null;
+
 dbAdd() {
     # dbAdd "database.record";
     sed -i "s/+ db.addnewrecord;/* ${1};\n+ db.addnewrecord;/g" /var/www/pterodactyl/.blueprint/db.md;
@@ -16,9 +19,7 @@ dbAdd() {
     sed -i "s/* ${1};//g" /var/www/pterodactyl/.blueprint/db.md;
 };
 
-cd /var/www/pterodactyl;
 touch /usr/local/bin/blueprint > /dev/null;
-mkdir -p .blueprint > /dev/null;
 echo -e "#!/bin/bash\nbash /var/www/pterodactyl/blueprint.sh \$@ -bash;" > /usr/local/bin/blueprint;
 chmod u+x /var/www/pterodactyl/blueprint.sh > /dev/null;
 chmod u+x /usr/local/bin/blueprint > /dev/null;
@@ -29,10 +30,21 @@ if [[ "$@" != *"-bash"* ]]; then
         dbRemove "blueprint.setupFinished";
     else
         echo -e $task"\033[0;33mMaking .blueprint files.\033[0m";
-
-        # CREATE DATABASE FILE
         touch .blueprint/db.md > /dev/null;
         echo -e "# Internal database for the bash side of Blueprint.\n+ db.addnewrecord;" > .blueprint/db.md;
+
+
+        echo -e $task"\033[0;33mInjecting custom css.\033[0m";
+        sed -i "s/@import 'checkbox.css';/@import 'checkbox.css';\n@import url(/assets/extensions/blueprint/blueprint.style.css);/g" /var/www/pterodactyl/public/themes/pterodactyl/css/pterodactyl.css;
+
+
+        echo -e $task"\033[0;33mClearing views.\033[0m";
+        php artisan view:clear > /dev/null;
+        php artisan config:clear > /dev/null;
+
+        echo -e $task"\033[0;33mUpdating permissions.\033[0m";
+        chown -R www-data:www-data /var/www/pterodactyl/* > /dev/null;
+
         dbAdd "blueprint.setupFinished";
     fi;
 fi;
