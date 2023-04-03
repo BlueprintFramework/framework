b48115ddcbda941f0d7d38359711cb8700e045f8
Jack G <J.gurling@hotmail.com>
2023-04-03 01:51:44 +0100

    Update blueprint.sh
    Undid previous change since installer bug is still happening

--- blueprint/lib/db.sh
+++ blueprint/lib/db.sh
@@ -2,15 +2,11 @@
 
 dbAdd() {
     # dbAdd "database.record";
-    sed -i "s/+ db.addnewrecord;/&\n* ${1};/" /var/www/pterodactyl/.blueprint/db.md > /dev/null
-}
-
-dbValidate() {
+    sed -i "s/+ db.addnewrecord;/* ${1};\n+ db.addnewrecord;/g" /var/www/pterodactyl/.blueprint/db.md > /dev/null;
+}; dbValidate() {
     # dbValidate "database.record";
-    grep -Fxq "* ${1};" /var/www/pterodactyl/.blueprint/db.md > /dev/null
-}
-
-dbRemove() {
+    grep -Fxq "* ${1};" /var/www/pterodactyl/.blueprint/db.md > /dev/null;
+}; dbRemove() {
     # dbRemove "database.record";
-    sed -i "/^\\* ${1};$/d" /var/www/pterodactyl/.blueprint/db.md > /dev/null
-}
+    sed -i "s/* ${1};//g" /var/www/pterodactyl/.blueprint/db.md > /dev/null;
+};
