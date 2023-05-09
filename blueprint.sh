cbdb076ef99d36bdbdb98bc23504b82122dd487b
purple <hello@prpl.wtf>
2023-05-09 22:07:29 +0200

    Update fallback version

--- blueprint.sh
+++ blueprint.sh
@@ -10,7 +10,7 @@ if [[ -f ".dockerenv" ]]; then
 fi;
 
 # If the fallback version below does not match your downloaded version, please let us know.
-  VER_FALLBACK="indev-8X2";
+  VER_FALLBACK="indev-AXN";
 
 # This will be automatically replaced by some marketplaces, if not, $VER_FALLBACK will be used as fallback.
   PM_VERSION="([(pterodactylmarket_version)])";
