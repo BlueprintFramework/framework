025d67e12313f997ac1e762b6bd9fd33f94b1fcc
purple <hello@prpl.wtf>
2023-05-09 20:37:25 +0200

    Missing semicolon

--- blueprint/lib/db.sh
+++ blueprint/lib/db.sh
@@ -3,7 +3,7 @@
 FLDR="/var/www/&bp.folder&/.blueprint/.storage/db.md";
 
 # If Blueprint does not replace the variable, default to "/var/www/pterodactyl" as root folder.
-if [[ $FLDR == "/var/www/&b""p.folder&/.blueprint/.storage/db.md" ]]; then FLDR="/var/www/pterodactyl/.blueprint/.storage/db.md" fi;
+if [[ $FLDR == "/var/www/&b""p.folder&/.blueprint/.storage/db.md" ]]; then FLDR="/var/www/pterodactyl/.blueprint/.storage/db.md"; fi;
 
 dbAdd() {
     # dbAdd "database.record";
