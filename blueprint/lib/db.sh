5b08dcd88a33b0d0ccb3a62bfbd32358068e1a86
purple <hello@prpl.wtf>
2023-05-09 18:06:44 +0200

    db.md is now located in the .storage folder.

--- blueprint/lib/db.sh
+++ blueprint/lib/db.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-FLDR="/var/www/&bp.folder&";
+FLDR="/var/www/&bp.folder&/.blueprint/.storage/db.md";
 
 # If Blueprint does not replace the variable, default to "/var/www/pterodactyl" as root folder.
-if [[ $FLDR == "/var/www/&b""p.folder&" ]]; then FLDR="/var/www/pterodactyl" fi;
+if [[ $FLDR == "/var/www/&b""p.folder&/.blueprint/.storage/db.md" ]]; then FLDR="/var/www/pterodactyl/.blueprint/.storage/db.md" fi;
 
 dbAdd() {
     # dbAdd "database.record";
-    sed -i "s/+ db.addnewrecord;/* ${1};\n+ db.addnewrecord;/g" $FLDR/.blueprint/db.md > /dev/null;
+    sed -i "s/+ db.addnewrecord;/* ${1};\n+ db.addnewrecord;/g" $FLDR > /dev/null;
 }; dbValidate() {
     # dbValidate "database.record";
-    grep -Fxq "* ${1};" $FLDR/.blueprint/db.md > /dev/null;
+    grep -Fxq "* ${1};" $FLDR > /dev/null;
 }; dbRemove() {
     # dbRemove "database.record";
-    sed -i "s/* ${1};//g" $FLDR/.blueprint/db.md > /dev/null;
+    sed -i "s/* ${1};//g" $FLDR > /dev/null;
 };
