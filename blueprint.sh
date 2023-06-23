03dcc77af6d0b3889f6059ff93c9f2878dc1fb51
purple <hello@prpl.wtf>
2023-06-23 10:33:24 +0200

    Update fallback version to latest.

--- blueprint.sh
+++ blueprint.sh
@@ -10,7 +10,7 @@ if [[ -f ".dockerenv" ]]; then
 fi;
 
 # If the fallback version below does not match your downloaded version, please let us know.
-  VER_FALLBACK="alpha-ML7";
+  VER_FALLBACK="alpha-5KB";
 
 # This will be automatically replaced by some marketplaces, if not, $VER_FALLBACK will be used as fallback.
   PM_VERSION="([(pterodactylmarket_version)])";
