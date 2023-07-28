07617942b8298a6c1c2e3e1cfa74e3cc87a86dc7
purple <hello@prpl.wtf>
2023-07-28 22:18:47 +0200

    Create Byte's thinking animation.

--- blueprint/lib/byte.sh
+++ blueprint/lib/byte.sh
@@ -15,93 +15,28 @@ byteBlink_1=" ╭         ╮   ";
 byteBlink_2=" │   -   - │   ";
 byteBlink_3=" ╰         ╯   ";
 
-byteSpawn() {
+byteThink() {
   clear;echo -e "
   ╭         ╮   _
-  │         │
-  ╰         ╯
-  "
-  sleep 0.5
-  clear;echo -e "
-  ╭         ╮   _
-  │   -   - │
-  ╰         ╯
-  "
-  sleep 1
-  clear;echo -e "
-  ╭         ╮   _
-  │   o   - │
-  ╰         ╯
-  "
-  sleep 0.2
-  clear;echo -e "
-  ╭         ╮   _
-  │   o   o │
-  ╰         ╯
-  "
-  sleep 0.5
-  clear;echo -e "
-  ╭         ╮   Hello_
-  │   o   o │
-  ╰         ╯
-  "
-  sleep 0.1
-  clear;echo -e "
-  ╭         ╮   Hello traveler_
-  │   o   o │
+  │ %^&!@*( │
   ╰         ╯
   "
   sleep 0.05
   clear;echo -e "
-  ╭         ╮   Hello traveler,_
-  │   o   o │
-  ╰         ╯
-  "
-  sleep 0.1
-  clear;echo -e "
-  ╭         ╮   Hello traveler,
-  │   o   o │   nice_
+  ╭         ╮   _
+  │ *#$&#@@ │
   ╰         ╯
   "
   sleep 0.05
   clear;echo -e "
-  ╭         ╮   Hello traveler,
-  │   ^   o │   nice to_
-  ╰         ╯
-  "
-  sleep 0.1
-  clear;echo -e "
-  ╭         ╮   Hello traveler,
-  │   ^   ^ │   nice to meet_
+  ╭         ╮   _
+  │ #^%&&** │
   ╰         ╯
   "
   sleep 0.05
-  clear;echo -e "
-  ╭         ╮   Hello traveler,
-  │   ^   ^ │   nice to meet you_
-  ╰         ╯
-  "
-  sleep 0.1
-  clear;echo -e "
-  ╭         ╮   Hello traveler,
-  │   ^   ^ │   nice to meet you!_
-  ╰         ╯
-  "
-  sleep 1
-  clear;echo -e "
-  ╭         ╮   Hello traveler,
-  │   -   - │   nice to meet you!_
-  ╰         ╯
-  "
-  sleep 0.07
-  clear;echo -e "
-  ╭         ╮   Hello traveler,
-  │   ^   ^ │   nice to meet you!_
-  ╰         ╯
-  "
-  sleep 2
+  byteThink;
 };
-byteSpawn;
+byteThink;
 byteIntroduction() {
 
 };
