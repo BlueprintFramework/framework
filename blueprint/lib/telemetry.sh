f8272635761f1d7d7569e60b4aa3354789ab80f8
purple <hello@prpl.wtf>
2023-08-11 21:56:03 +0200

    Fix incorrect path.

--- blueprint/lib/telemetry.sh
+++ blueprint/lib/telemetry.sh
@@ -4,6 +4,7 @@
 # and may be intergrated directly into the core in the future. 
 
 sendTelemetry() {
+  cd &bp.folder&;
   key=$(cat .blueprint/data/internal/db/telemetry_id);
   if [[ $key == "KEY_NOT_UPDATED" ]]; then exit 1;fi;
   curl --location --silent "http://data.ptero.shop:3481/send/$key/$1" > /dev/null;
--- blueprint/lib/updateAdminCacheReminder.sh
+++ blueprint/lib/updateAdminCacheReminder.sh
@@ -6,6 +6,6 @@ updateCacheReminder() {
   oldClassName=$(cat .blueprint/data/internal/db/randomclassname);
   newClassName=$RANDOM;
   sed -i "s~$oldClassName~$newClassName~g" public/assets/extensions/blueprint/adminCacheReminderHider.css;
-  sed -i "s~$oldClassName~$newClassName~g" resources/layouts/admin.blade.php;
+  sed -i "s~$oldClassName~$newClassName~g" resources/views/layouts/admin.blade.php;
   echo "$newClassName" > .blueprint/data/internal/db/randomclassname;
 }
