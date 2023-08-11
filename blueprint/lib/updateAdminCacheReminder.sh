360770809671d774915fe4fcbe4d12c61703c9c6
purple <hello@prpl.wtf>
2023-08-11 22:01:01 +0200

    Fix accidentally replacing everything with the number one on the admin panel.

--- blueprint/lib/updateAdminCacheReminder.sh
+++ blueprint/lib/updateAdminCacheReminder.sh
@@ -5,7 +5,7 @@ updateCacheReminder() {
   # Overwrite previous adminCacheReminderHider with the default one.
   oldClassName=$(cat .blueprint/data/internal/db/randomclassname);
   newClassName=$RANDOM;
-  sed -i "s~$oldClassName~$newClassName~g" public/assets/extensions/blueprint/adminCacheReminderHider.css;
-  sed -i "s~$oldClassName~$newClassName~g" resources/views/layouts/admin.blade.php;
+  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" public/assets/extensions/blueprint/adminCacheReminderHider.css;
+  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" resources/views/layouts/admin.blade.php;
   echo "$newClassName" > .blueprint/data/internal/db/randomclassname;
 }
