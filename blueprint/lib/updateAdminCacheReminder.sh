0746fc80df1decf1a6ee9ceec60c6afdf8b9fa46
purple <hello@prpl.wtf>
2023-08-13 16:37:37 +0200

    Fix cache reminder not working on most browsers.

--- blueprint/lib/updateAdminCacheReminder.sh
+++ blueprint/lib/updateAdminCacheReminder.sh
@@ -5,7 +5,9 @@ updateCacheReminder() {
   # Overwrite previous adminCacheReminderHider with the default one.
   oldClassName=$(cat .blueprint/data/internal/db/randomclassname);
   newClassName=$RANDOM;
+  mv public/assets/extensions/blueprint/misc/cacheOverlay-$oldClassName public/assets/extensions/blueprint/misc/cacheOverlay-$newClassName;
   sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" public/assets/extensions/blueprint/blueprint.style.css;
   sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" resources/views/layouts/admin.blade.php;
+  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" public/assets/extensions/blueprint/misc/cacheOverlay-$newClassName;
   echo "$newClassName" > .blueprint/data/internal/db/randomclassname;
 }
