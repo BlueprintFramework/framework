f82478852f12acf54bdf67d93478aaf0f4c2962b
purple <hello@prpl.wtf>
2023-08-31 18:21:07 +0200

    Attempt at fixing syntax error caused by unfinished command.

--- blueprint.sh
+++ blueprint.sh
@@ -19,7 +19,7 @@
 # Allow non-default Pterodactyl installation folders.
 if [[ $_FOLDER != "" ]]; then
   if [[ ( ! -f "$FOLDER/.blueprint/data/internal/db/version" ) || ( $FOLDER == "/var/www/pterodactyl" ) ]];
-    sed -i -E "s|FOLDER=\"$FOLDER\" #;|FOLDER=\"$_FOLDER\" #;|g"
+    sed -i -E "s|FOLDER=\"$FOLDER\" #;|FOLDER=\"$_FOLDER\" #;|g" $_FOLDER/blueprint.sh
   else
     echo "Variable cannot be replaced right now."
   fi
