8b15d1577b0f4a0425734a7c7df08e0f162031b8
purple <hello@prpl.wtf>
2023-09-13 21:31:19 +0200

    Import BlueprintClientLibrary directly in the client wrapper instead of BaseController.

--- resources/views/templates/wrapper.blade.php
+++ resources/views/templates/wrapper.blade.php
@@ -1,3 +1,8 @@
+@php
+    use Pterodactyl\BlueprintFramework\Libraries\ExtensionLibrary\Client\BlueprintClientLibrary as BlueprintExtensionLibrary;
+    $settings = app()->make('Pterodactyl\Contracts\Repository\SettingsRepositoryInterface');
+    $blueprint = app()->make(BlueprintExtensionLibrary::class, ['settings' => $settings]);
+@endphp
 <!DOCTYPE html>
 <html>
     <head>
