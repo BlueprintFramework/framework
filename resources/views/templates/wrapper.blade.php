ae5873b8b8b2619ad3cade43f3da292238eaef87
purple <hello@prpl.wtf>
2023-09-25 11:40:54 +0200

    Yield metatags section to allow extensions to add meta tags in the wrapper.

--- resources/views/templates/wrapper.blade.php
+++ resources/views/templates/wrapper.blade.php
@@ -22,6 +22,7 @@
             <link rel="shortcut icon" href="/favicons/favicon.ico">
             <meta name="msapplication-config" content="/favicons/browserconfig.xml">
             <meta name="theme-color" content="#0e4688">
+            @yield('metatags')
         @show
 
         @section('user-data')
