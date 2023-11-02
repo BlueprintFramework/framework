8d979ddc4fb1a79bb320d1affaca01d65fd29438
purple <hello@prpl.wtf>
2023-11-02 19:14:06 +0100

    feat(core): Add new -info command that shows info about your Blueprint install, inspired by neofetch.

--- blueprint.sh
+++ blueprint.sh
@@ -906,6 +906,7 @@ ${help_dev_primary}Developer${help_dev_status}\x1b[0m${help_dev_secondary}
   
 \x1b[34;1mMisc\x1b[0m\x1b[34m
   -version             -v  returns the blueprint version
+  -info                -f  show neofetch-like information about blueprint
   \x1b[0m
   
 \x1b[34;1mAdvanced\x1b[0m\x1b[34m
@@ -1193,15 +1194,40 @@ if [[ ( $2 == "-wipe" || $2 == "-w" ) ]]; then VCMD="y"
   log_green "[SUCCESS] Your development files have been removed."
 fi
 
+# -info
+if [[ ( $2 == "-info" || $2 == "-f" ) ]]; then VCMD="y"
+  fetchversion() { log_reset log_white $VERSION; }
+  fetchfolder() { log_reset log_white $FOLDER; }
+  fetchurl() { log_reset log_white $(grabAppUrl); }
+  fetchlocale() { log_reset log_white $(grabAppLocale); }
+  fetchtimezone() { log_reset log_white $(grabAppTimezone); }
+  fetchdeveloper() { log_reset log_white $(if dbValidate "blueprint.developerEnabled"; then echo "true"; else echo "false"; fi;); }
+  fetchtelemetry() { log_reset log_white $(telemetrykey=$(cat .blueprint/data/internal/db/telemetry_id); if [[ $telemetrykey == "KEY_NOT_UPDATED" ]]; then echo "false"; else echo "true"; fi;); }
+  fetchextensions() { log_reset log_white $(echo "$(<.blueprint/data/internal/db/installed_extensions)" | tr -cd ',' | wc -c | tr -d ' '); }
+  fetchnode() { log_reset log_white $(node -v); }
+  fetchyarn() { log_reset log_white $(yarn -v); }
+
+  log_bright " "
+  log_bright "  ⣿⣿    $(log_reset log_bold log_blue Version:) $(fetchversion)"
+  log_bright "⣿⣿  ⣿⣿  $(log_reset log_bold log_blue Folder:) $(fetchfolder)"
+  log_bright "  ⣿⣿⣿⣿  $(log_reset log_bold log_blue URL:) $(fetchurl)"
+  log_bright "        $(log_reset log_bold log_blue Locale:) $(fetchlocale)"
+  log_bright "        $(log_reset log_bold log_blue Timezone:) $(fetchtimezone)"
+  log_bright "        $(log_reset log_bold log_blue Developer:) $(fetchdeveloper)"
+  log_bright "        $(log_reset log_bold log_blue Telemetry:) $(fetchtelemetry)"
+  log_bright "        $(log_reset log_bold log_blue Extensions:) $(fetchextensions)"
+  log_bright "        $(log_reset log_bold log_blue Node:) $(fetchnode)"
+  log_bright "        $(log_reset log_bold log_blue Yarn:) $(fetchyarn)"
+  log_bright " "
+fi
 
 # -rerun-install
-if [[ $2 == "-rerun-install"  ]]; then VCMD="y"
+if [[ $2 == "-rerun-install" ]]; then VCMD="y"
   log_yellow "[WARNING] This is an advanced feature, only proceed if you know what you are doing.\n"
   dbRemove "blueprint.setupFinished"
   cd ${FOLDER}
   bash blueprint.sh
 fi
-
 
 # -upgrade
 if [[ $2 == "-upgrade" ]]; then VCMD="y"
