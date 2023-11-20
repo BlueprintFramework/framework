b48b083a640db11052141d08696384dad249658d
purple <hello@prpl.wtf>
2023-11-20 23:48:20 +0100

    feat(lib-cachereminder): make code more readable

--- blueprint/lib/updateAdminCacheReminder.sh
+++ blueprint/lib/updateAdminCacheReminder.sh
@@ -3,11 +3,11 @@
 updateCacheReminder() {
   cd $BLUEPRINT__FOLDER
   # Overwrite previous adminCacheReminderHider with the default one.
-  oldClassName=$(cat .blueprint/extensions/blueprint/private/db/randomclassname);
-  newClassName=$RANDOM;
-  mv .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$oldClassName.css .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$newClassName.css;
-  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" .blueprint/extensions/blueprint/assets/blueprint.style.css;
-  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" resources/views/layouts/admin.blade.php;
-  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$newClassName.css;
-  echo "$newClassName" > .blueprint/extensions/blueprint/private/db/randomclassname;
+  oldClassName=$(cat .blueprint/extensions/blueprint/private/db/randomclassname)
+  newClassName=$RANDOM
+  mv .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$oldClassName.css .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$newClassName.css
+  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" .blueprint/extensions/blueprint/assets/blueprint.style.css
+  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" resources/views/layouts/admin.blade.php
+  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$newClassName.css
+  echo "$newClassName" > .blueprint/extensions/blueprint/private/db/randomclassname
 }
