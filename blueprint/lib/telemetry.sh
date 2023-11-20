040c0465dc0aee9de0a2c3dc3c1efb352c9a64a7
purple <hello@prpl.wtf>
2023-11-20 23:49:24 +0100

    feat(lib-telemetry): improve code readability

--- blueprint/lib/telemetry.sh
+++ blueprint/lib/telemetry.sh
@@ -4,8 +4,10 @@
 # and may be intergrated directly into the core in the future. 
 
 sendTelemetry() {
-  cd $BLUEPRINT__FOLDER;
-  key=$(cat .blueprint/extensions/blueprint/private/db/telemetry_id);
-  if [[ $key == "KEY_NOT_UPDATED" ]]; then exit 1;fi;
-  curl --location --silent "http://data.ptero.shop:3481/send/$key/$1" > /dev/null;
+  cd $BLUEPRINT__FOLDER
+  key=$(cat .blueprint/extensions/blueprint/private/db/telemetry_id)
+  if [[ $key == "KEY_NOT_UPDATED" ]]; then 
+    exit 1
+  fi
+  curl --location --silent "http://data.ptero.shop:3481/send/$key/$1" > /dev/null
 }
