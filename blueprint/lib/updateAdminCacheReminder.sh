83905266caa1fca723983f9fb1e57419dd68419b
prplwtf <hello@prpl.wtf>
2023-12-03 18:53:19 +0100

    fix(admincache): admin cache refresh does not disappear

--- blueprint/lib/updateAdminCacheReminder.sh
+++ blueprint/lib/updateAdminCacheReminder.sh
@@ -6,7 +6,7 @@ updateCacheReminder() {
   oldClassName=$(cat .blueprint/extensions/blueprint/private/db/randomclassname)
   newClassName=$RANDOM
   mv .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$oldClassName.css .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$newClassName.css
-  sed -i "s~I0TWHOPKAB-$oldClassName~I0TWHOPKAB-$newClassName~g" .blueprint/extensions/blueprint/assets/blueprint.style.css
+  sed -i "s~cacheOverlay-$oldClassName~cacheOverlay-$newClassName~g" .blueprint/extensions/blueprint/assets/blueprint.style.css
   sed -i "s~I0TWHOPKAB-$oldClassName~I0TWHOPKAB-$newClassName~g" resources/views/layouts/admin.blade.php
   sed -i "s~I0TWHOPKAB-$oldClassName~I0TWHOPKAB-$newClassName~g" .blueprint/extensions/blueprint/assets/misc/cacheOverlay-$newClassName.css
   echo "$newClassName" > .blueprint/extensions/blueprint/private/db/randomclassname
