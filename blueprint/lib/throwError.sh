a3c9dda69d41a3944f6f984bce854b1d5b1cfd1c
prplwtf <hello@prpl.wtf>
2024-01-15 22:10:22 +0100

    feat `throwError`: Remove multiple unused log definitions.

--- blueprint/lib/throwError.sh
+++ blueprint/lib/throwError.sh
@@ -12,11 +12,6 @@ function throwError {
   if [[ $1 == "confymlMissingFiles"     ]]; then err="A conf.yml value is pointing to a file that does not exist.";fi
   if [[ $1 == "scriptsMissingFiles"     ]]; then err="Could not find install/remove/export script even though it's enabled.";fi
   if [[ $1 == "scriptsNoDataDir"        ]]; then err="Could not run extension's install/remove/export script as the extension does not have a data directory.";fi
-  if [[ $1 == "debugLineCount"          ]]; then err="Provide the amount of debug lines to print as an argument, which must be greater than one (1).";fi
-  if [[ $1 == "componentFileExtension"  ]]; then err="Defined component paths may not end with a file extension.";fi
-  if [[ $1 == "missingComponentFiles"   ]]; then err="One or more components defined in 'Components.yml' could not be found.";fi
-  if [[ $1 == "componentEscape"         ]]; then err="Component file paths must not escape the temporarily extension directory.";fi
-  if [[ $1 == "pathsEscape"             ]]; then err="File paths must not escape the temporarily extension directory.";fi
 
   if [[ $err == "" ]]; then err="$1"; fi
   PRINT FATAL "$err"
