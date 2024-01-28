d43be15f6651bd6710c4fd280c066649ffc2c572
prplwtf <hello@prpl.wtf>
2024-01-28 22:25:06 +0100

    feat `core`: create build file when routes

--- blueprint.sh
+++ blueprint.sh
@@ -761,7 +761,13 @@ if [[ ( $2 == "-i" ) || ( $2 == "-install" ) ]]; then VCMD="y"
 
 
           # apply routes..
-
+          if [[ $COMPONENTS_ROUTE_TYPE == "server" ]]; then
+            # copy build file
+            cp .blueprint/extensions/blueprint/private/build/extensions/clientServerRoute .blueprint/extensions/blueprint/private/build/extensions/clientServerRoute.bak
+          elif [[ $COMPONENTS_ROUTE_TYPE == "account" ]]; then
+            # copy build file
+            cp .blueprint/extensions/blueprint/private/build/extensions/clientAccountRoute .blueprint/extensions/blueprint/private/build/extensions/clientAccountRoute.bak
+          fi 
 
           # Clear variables after doing all route stuff for a defined route.
           COMPONENTS_ROUTE_NAME=""
