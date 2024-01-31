11f36cd64d829cd94cae6d5f0f0571b264112b2e
prplwtf <hello@prpl.wtf>
2024-01-31 17:40:40 +0100

    feat `routes`: Change formatting.

--- resources/scripts/blueprint/extends/routers/routes.ts
+++ resources/scripts/blueprint/extends/routers/routes.ts
@@ -2,19 +2,9 @@ import React from 'react';
 
 /* blueprint/import */
 
-interface ExtendedRouteDefinition {
-  path: string;
-  name: string | undefined;
-  component: React.ComponentType;
-  exact?: boolean;
-}
-interface ExtendedServerRouteDefinition extends ExtendedRouteDefinition {
-  permission: string | string[] | null;
-}
-interface Routes {
-  account: ExtendedRouteDefinition[];
-  server: ExtendedServerRouteDefinition[];
-}
+interface ExtendedRouteDefinition { path: string; name: string | undefined; component: React.ComponentType; exact?: boolean; }
+interface ExtendedServerRouteDefinition extends ExtendedRouteDefinition { permission: string | string[] | null; }
+interface Routes { account: ExtendedRouteDefinition[]; server: ExtendedServerRouteDefinition[]; }
 
 export default {
   account: [
