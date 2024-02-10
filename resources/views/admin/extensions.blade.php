a742f6cbb16a5479b65d7cda5aeaeacc317b933f
purple <hello@prpl.wtf>
2024-02-11 00:23:59 +0100

    feat `views` `extensions.blade.php`: Modify page description

--- resources/views/admin/extensions.blade.php
+++ resources/views/admin/extensions.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content-header')
-  <h1>Extensions<small>Manage all your installed extensions.</small></h1>
+  <h1>Extensions<small>Manage and configure all of your installed extensions.</small></h1>
   <ol class="breadcrumb">
     <li><a href="{{ route('admin.index') }}">Admin</a></li>
     <li class="active">Extensions</li>
