6be17ca4a660c379711f3bdc3826dedb16652b5e
prplwtf <hello@prpl.wtf>
2024-02-27 22:46:54 +0100

    fix `core`: -debug threw random errors when receiving text input

--- blueprint.sh
+++ blueprint.sh
@@ -1605,6 +1605,7 @@ fi
 
 # -debug
 if [[ $2 == "-debug" ]]; then VCMD="y"
+  if ! [[ $3 =~ [0-9] ]] && [[ $3 != "" ]]; then PRINT FATAL "Amount of debug lines must be a number."; exit 2; fi
   if [[ $3 -lt 1 ]]; then PRINT FATAL "Provide the amount of debug lines to print as an argument, which must be greater than one (1)."; exit 2; fi
   echo -e "\x1b[30;47;1m  --- DEBUG START ---  \x1b[0m"
   echo -e "$(v="$(<.blueprint/extensions/blueprint/private/debug/logs.txt)";printf -- "%s" "$v"|tail -"$3")"
