3d4702eff18249edde4884347fc063c58ac123b5
prplwtf <hello@prpl.wtf>
2024-03-13 21:59:45 +0100

    feat `app`: make redirecting work for ExtensionConfigurationController on save

--- app/BlueprintFramework/Controllers/ExtensionConfigurationController.php
+++ app/BlueprintFramework/Controllers/ExtensionConfigurationController.php
@@ -4,6 +4,7 @@ namespace Pterodactyl\BlueprintFramework\Controllers;
 
 use Pterodactyl\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
+use Pterodactyl\Http\Requests\Admin\AdminFormRequest;
 
 class ExtensionConfigurationController extends Controller
 {
@@ -16,9 +17,24 @@ class ExtensionConfigurationController extends Controller
    * @throws \Pterodactyl\Exceptions\Model\DataValidationException
    * @throws \Pterodactyl\Exceptions\Repository\RecordNotFoundException
    */
-  public function update(): RedirectResponse
+  public function update(ExtensionConfigurationRequest $request): RedirectResponse
   {
     //foreach ($request->normalize() as $key => $value) { $this->settings->set('blueprint::' . $key, $value); }
     return redirect()->route('admin.extensions.'.$request->input('_identifier', 'blueprint').'.index');
+  }
+}
+
+class ExtensionConfigurationRequest extends AdminFormRequest
+{
+  public function rules(): array {
+    return [
+      '*' => 'nullable',
+    ];
+  }
+
+  public function attributes(): array {
+    return [
+      '*' => 'test',
+    ];
   }
 }
