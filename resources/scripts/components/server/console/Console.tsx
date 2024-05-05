255d6bce405218996a064559714096b8d845346a
Gasuge <warwax2003@gmail.com>
2024-05-05 03:19:16 +0200

    change component name ConsoleCommandRow to CommandRow

--- resources/scripts/components/server/console/Console.tsx
+++ resources/scripts/components/server/console/Console.tsx
@@ -16,7 +16,7 @@ import { SocketEvent, SocketRequest } from '@/components/server/events';
 import classNames from 'classnames';
 import { ChevronDoubleRightIcon } from '@heroicons/react/solid';
 
-import ConsoleCommandRow  from '@/blueprint/components/Server/Terminal/ConsoleCommandRow';
+import CommandRow  from '@/blueprint/components/Server/Terminal/CommandRow';
 
 import 'xterm/css/xterm.css';
 import styles from './style.module.css';
@@ -223,7 +223,7 @@ export default () => {
                     >
                         <ChevronDoubleRightIcon className={'w-4 h-4'} />
                     </div>
-                    <ConsoleCommandRow />
+                    <CommandRow />
                 </div>
             )}
         </div>
