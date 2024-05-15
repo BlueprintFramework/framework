30fc65da350a089691c3bc5d2a3a4319a4079fc4
prplwtf <hello@prpl.wtf>
2024-05-15 13:16:53 +0200

    feat `core` `artisan`: admit defeat on dynamic imports for now

--- config/app.php
+++ config/app.php
@@ -190,12 +190,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
-         * Blueprint Service Providers
-         */
-        Pterodactyl\Providers\Blueprint\RouteServiceProvider::class,
-        Pterodactyl\Providers\Blueprint\ExtensionCommandServiceProvider::class,
-
-        /*
          * Application Service Providers...
          */
         Pterodactyl\Providers\ActivityLogServiceProvider::class,
@@ -208,6 +202,11 @@ return [
         Pterodactyl\Providers\RouteServiceProvider::class,
         Pterodactyl\Providers\RepositoryServiceProvider::class,
         Pterodactyl\Providers\ViewComposerServiceProvider::class,
+
+        /*
+         * Blueprint Service Providers
+         */
+        Pterodactyl\Providers\Blueprint\RouteServiceProvider::class,
 
         /*
          * Additional Dependencies
