7b731e93c6bc50f24775e7439bbfd911eb5c29bd
Victor B. <contact@itsvic.dev>
2024-06-22 00:45:16 +0200

    feat `core`: KernelExtensions class for ivy

---
+++ app/BlueprintFramework/Console/KernelExtensions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pterodactyl\BlueprintFramework\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+
+class KernelExtensions {
+  public static function schedules(Schedule $schedule) {
+    /* blueprint/schedule */
+  }
+};
