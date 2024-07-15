40a2ec186b9b8f7b0640b528ba009f812b41f949
Jelco <53396500+Jelcoo@users.noreply.github.com>
2024-07-15 22:12:14 +0200

    fix (`logFormat.sh`): overwriting variables used elsewhere

--- scripts/libraries/logFormat.sh
+++ scripts/libraries/logFormat.sh
@@ -4,21 +4,21 @@
 # and uses the same license as the rest of the codebase.
 
 PRINT() {
-  DATE=$(date +"%H:%M:%S")
-  DATEDEBUG=$(date +"%Y-%m-%d %H:%M:%S")
-  TYPE="$1"
-  MESSAGE="$2"
+  local DATE=$(date +"%H:%M:%S")
+  local DATEDEBUG=$(date +"%Y-%m-%d %H:%M:%S")
+  local TYPE="$1"
+  local MESSAGE="$2"
 
-  BOLD=$(tput bold)
-  RESET=$(tput sgr0)
-  SECONDARY="\033[2m"
+  local BOLD=$(tput bold)
+  local RESET=$(tput sgr0)
+  local SECONDARY="\033[2m"
 
-  if [[ $TYPE == "INFO"    ]]; then ICON="󰋼"; READABLETYPE="Info"; PRIMARY=$(tput setaf 4); fi
-  if [[ $TYPE == "WARNING" ]]; then ICON=""; READABLETYPE="Warning"; PRIMARY=$(tput setaf 3); fi
-  if [[ $TYPE == "FATAL"   ]]; then ICON="󰅙"; READABLETYPE="Fatal"; PRIMARY=$(tput setaf 1); fi
-  if [[ $TYPE == "SUCCESS" ]]; then ICON="󰗠"; READABLETYPE="Success"; PRIMARY=$(tput setaf 2); fi
-  if [[ $TYPE == "INPUT"   ]]; then ICON="󰋗"; READABLETYPE="Input"; PRIMARY=$(tput setaf 5); fi
-  if [[ $TYPE == "DEBUG"   ]]; then PRIMARY="$SECONDARY"; fi
+  if [[ $TYPE == "INFO"    ]]; then local ICON="󰋼"; local READABLETYPE="Info"; local PRIMARY=$(tput setaf 4); fi
+  if [[ $TYPE == "WARNING" ]]; then local ICON=""; local READABLETYPE="Warning"; local PRIMARY=$(tput setaf 3); fi
+  if [[ $TYPE == "FATAL"   ]]; then local ICON="󰅙"; local READABLETYPE="Fatal"; local PRIMARY=$(tput setaf 1); fi
+  if [[ $TYPE == "SUCCESS" ]]; then local ICON="󰗠"; local READABLETYPE="Success"; local PRIMARY=$(tput setaf 2); fi
+  if [[ $TYPE == "INPUT"   ]]; then local ICON="󰋗"; local READABLETYPE="Input"; local PRIMARY=$(tput setaf 5); fi
+  if [[ $TYPE == "DEBUG"   ]]; then local PRIMARY="$SECONDARY"; fi
 
   if [[ $TYPE != "DEBUG" ]]; then echo -e "${SECONDARY}${DATE}${RESET} ${PRIMARY}${TYPE}:${RESET} $MESSAGE${RESET}"; fi
   echo -e "${BOLD}${SECONDARY}$DATEDEBUG${RESET} ${PRIMARY}${TYPE}:${RESET} $MESSAGE" >> "$FOLDER"/.blueprint/extensions/blueprint/private/debug/logs.txt
