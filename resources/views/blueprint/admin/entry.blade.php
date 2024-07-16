0c9773d7815c6e93ba9ff26c791220b50767ba1d
prplwtf <hello@prpl.wtf>
2024-07-16 15:26:36 +0200

    feat `entry`: add alt text to extension icons

--- resources/views/blueprint/admin/entry.blade.php
+++ resources/views/blueprint/admin/entry.blade.php
@@ -3,8 +3,8 @@
     <a href="{{ route('admin.extensions.'.$EXTENSION_ID.'.index') }}">
       <button class="btn extension-btn" style="width:100%;margin-bottom:17px;">
         <div class="extension-btn-overlay"></div>
-        <img src="{{ $EXTENSION_ICON }}" alt="logo" class="extension-btn-image2"/>
-        <img src="{{ $EXTENSION_ICON }}" alt="logo" class="extension-btn-image"/>
+        <img src="{{ $EXTENSION_ICON }}" alt="{{ $EXTENSION_ID }}" class="extension-btn-image2"/>
+        <img src="{{ $EXTENSION_ICON }}" alt="" class="extension-btn-image"/>
         <p class="extension-btn-text">{{ $EXTENSION_NAME }}</p>
         <p class="extension-btn-version">{{ $EXTENSION_VERSION }}</p>
         <i class="bi bi-arrow-right-short" style="font-size: 34px;position: absolute;top: 15px;right: 30px;"></i>
