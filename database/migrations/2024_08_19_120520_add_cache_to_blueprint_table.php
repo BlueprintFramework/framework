8fc55a75b37665a869266a3f59394ca0180f5df7
prplwtf <hello@prpl.wtf>
2024-08-19 12:33:56 +0200

    fix `AddCacheToBlueprintTable`: incorrect class

--- database/migrations/2024_08_19_120520_add_cache_to_blueprint_table.php
+++ database/migrations/2024_08_19_120520_add_cache_to_blueprint_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBlueprintTable extends Migration
+class AddCacheToBlueprintTable extends Migration
 {
   /**
    * Run the migrations.
