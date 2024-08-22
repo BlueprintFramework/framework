972c60e6207e116b3201e7bb552a88fa0d9a8362
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2024-08-22 16:47:33 +0200

    feat `build-files` `route.php`: add parameters to delete & name routes

--- blueprint/extensions/blueprint/private/build/extensions/route.php
+++ blueprint/extensions/blueprint/private/build/extensions/route.php
@@ -1,1 +1,7 @@
-Route::group(['prefix' => 'extensions/[id]'], function () {Route::get('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'index'])->name('admin.extensions.[id].index');Route::patch('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'update']);Route::post('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'post']);Route::put('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'put']);Route::delete('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'delete']);Route::options('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'options']);});
+Route::group(['prefix' => 'extensions/[id]'], function () {
+    Route::get('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'index'])->name('admin.extensions.[id].index');
+    Route::patch('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'update'])->name('admin.extensions.[id].patch');
+    Route::post('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'post'])->name('admin.extensions.[id].post');
+    Route::put('/', [Admin\Extensions\[id]\[id]ExtensionController::class, 'put'])->name('admin.extensions.[id].put');
+    Route::delete('/{target}/{id}', [Admin\Extensions\[id]\[id]ExtensionController::class, 'delete'])->name('admin.extensions.[id].delete');
+});
