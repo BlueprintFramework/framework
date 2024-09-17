2824f0fd69842abcbf6ab14a5479dc0d08ec6e04
Ivy <hello@prpl.wtf>
2024-09-17 21:45:32 +0200

    feat `LICENSE`: Update license holder name

--- blueprint.sh
+++ blueprint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# © 2023-2024 Ivy (prpl.wtf)
+# © 2023-2024 Emma (prpl.wtf)
 
 # Learn more @ blueprint.zip
 # Source code available at github.com/blueprintframework/framework
@@ -224,7 +224,7 @@ if [[ $1 != "-bash" ]]; then
       echo -e "$C0" \
         "\n$C4  ██$C1▌$C2▌$C3▌$C0   Blueprint Framework" \
         "\n$C4██  ██$C1▌$C2▌$C3▌$C0 https://blueprint.zip" \
-        "\n$C4  ████$C1▌$C2▌$C3▌$C0 © 2023-2024 Ivy (prpl.wtf)\n";
+        "\n$C4  ████$C1▌$C2▌$C3▌$C0 © 2023-2024 Emma (prpl.wtf)\n";
     fi
 
     PRINT INFO "Searching and validating framework dependencies.."
