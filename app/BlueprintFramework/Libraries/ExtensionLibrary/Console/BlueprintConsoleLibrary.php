99c97653b4360bcc4fa45ac7b93338a8c4e92598
Emma <hello@prpl.wtf>
2024-10-15 13:43:02 +0200

    feat: add docblock head to consolelibrary

--- app/BlueprintFramework/Libraries/ExtensionLibrary/Console/BlueprintConsoleLibrary.php
+++ app/BlueprintFramework/Libraries/ExtensionLibrary/Console/BlueprintConsoleLibrary.php
@@ -1,7 +1,16 @@
 <?php
 
-// Core file for the console library for Blueprint Extensions
-
+/**
+ * BlueprintExtensionLibrary (Console variation)
+ *
+ * @category   BlueprintExtensionLibrary
+ * @package    BlueprintConsoleLibrary
+ * @author     Emma <hello@prpl.wtf>
+ * @copyright  2023-2024 Emma (prpl.wtf)
+ * @license    https://blueprint.zip/docs/?page=about/License MIT License
+ * @link       https://blueprint.zip/docs/?page=documentation/$blueprint
+ * @since      beta
+ */
 
 namespace Pterodactyl\BlueprintFramework\Libraries\ExtensionLibrary\Console;
 
