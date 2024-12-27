e36449231c2bd2856b685946b765f915e7ac5430
Emma <hello@prpl.wtf>
2024-12-27 21:39:29 +0100

    feat `query`: do work in progress query.sh stuff or something

--- scripts/commands/extensions/query.sh
+++ scripts/commands/extensions/query.sh
@@ -3,12 +3,9 @@
 Command() {
   if [[ $1 == "" ]]; then PRINT FATAL "Expected 1 argument but got 0.";exit 2;fi
   
-  PRINT INFO "Searching and validating framework dependencies.."
   # Check if required programs and libraries are installed.
   depend
 
-  PRINT INFO "Extracting extension.."
-  dev=false
   n="$1"
 
   if [[ $n == *".blueprint" ]]; then n="${n::-10}";fi
@@ -48,10 +45,9 @@ Command() {
 
   eval "$(parse_yaml .blueprint/tmp/"${n}"/conf.yml conf_)"
 
-  echo $conf__
-
   echo -e \
-  "\x1b[37;1mnebula.blueprint\x1b[0m"
+  "\x1b[1m$conf_info_name \033[0;2m(nebula.blueprint)"\
+  "\n$conf_info_description\033[0m"
 
   rm -R ".blueprint/tmp/$n"
 
