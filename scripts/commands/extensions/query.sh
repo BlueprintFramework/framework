4421ecca3cb30620b1869faa5f64b9f151c27803
Emma <hello@prpl.wtf>
2024-12-28 14:20:55 +0100

    feat `query.sh`: list extension apis

--- scripts/commands/extensions/query.sh
+++ scripts/commands/extensions/query.sh
@@ -36,6 +36,47 @@ Command() {
     "\x1b[31;1m  Utilizes extension scripts, which can be a safety risk. Extensions with installation scripts can cause conflicts and break other extensions. Learn more at \033[0;2mblueprint.zip/docs/?page=documentation/scripts\x1b[0;31;1m.\033[0m"
   fi
 
+  # Extension APIs
+  echo -e "\n\x1b[1mExtension APIs:\033[0m"
+
+  local extension_apis
+  for config_category in $conf__; do
+    # $config_category is conf_info, conf_admin, etc. here
+    config_category_="${config_category}_" # Contents of $config_category
+
+    if [[ ${config_category} != "conf_info" ]]; then
+      for config_item in ${!config_category_}; do
+        # $config_item is conf_info_name, conf_info_identifier, etc. here
+        config_item_clean="${config_item//conf_/}" # Removed "conf_" from variable content
+        config_item_="${config_item}_" # conf_requests_routers_ for example
+
+        if [[ ${!config_item_} != "" ]]; then
+          local has_suboption=false
+          # If extension api has suboptions, go through them
+          for config_suboption in ${!config_item_}; do
+            if [[ ${!config_suboption} != "" ]]; then
+              has_suboption=true
+            fi
+          done
+          if $has_suboption; then
+            extension_apis+="${config_item_clean} "
+          fi
+        else
+          PRINT DEBUG "${config_item_clean} :: ${!config_item}"
+
+          if [[ "${!config_item}" != "" ]]; then
+            extension_apis+="${config_item_clean} "
+          fi
+        fi
+      done
+    fi
+  done
+  extension_apis=${extension_apis// /, }
+  # shellcheck disable=SC2001
+  extension_apis="$(echo "$extension_apis" | sed 's/,\([^,]*\)$/ \1/')"
+
+  echo -e "$extension_apis"
+
   rm -R ".blueprint/tmp/$n"
   mkdir -p .blueprint/tmp
 
