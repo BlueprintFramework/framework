f38c7fefd14788f3e57ff1a12c878837e6ed4afe
Emma <hello@prpl.wtf>
2025-02-12 20:45:36 +0100

    fix `tsconfig.json` `webpack.config.js`: alias thingies

--- webpack.config.js
+++ webpack.config.js
@@ -133,8 +133,6 @@ module.exports = {
       '@': path.join(__dirname, '/resources/scripts'),
       '@definitions': path.join(__dirname, '/resources/scripts/api/definitions'),
       '@feature': path.join(__dirname, '/resources/scripts/components/server/features'),
-
-      // Blueprint aliases
       '@blueprint': path.join(__dirname, '/resources/scripts/blueprint'),
       '@extensions': path.join(__dirname, '/resources/scripts/blueprint/extensions'),
     },
