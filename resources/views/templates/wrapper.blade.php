cc0919a9d84d08ea3a12001a449b82cbcbd41c95
prplwtf <hello@prpl.wtf>
2026-01-13 01:53:32 +0100

    feat: update admin wrapper

--- resources/views/templates/wrapper.blade.php
+++ resources/views/templates/wrapper.blade.php
@@ -36,10 +36,6 @@
                 </script>
             @endif
         @show
-        <style>
-            @import url('//fonts.googleapis.com/css?family=Rubik:300,400,500&display=swap');
-            @import url('//fonts.googleapis.com/css?family=IBM+Plex+Mono|IBM+Plex+Sans:500&display=swap');
-        </style>
 
         @yield('assets')
 
@@ -50,7 +46,7 @@
             @yield('above-container')
             @yield('container')
             @yield('below-container')
-            
+
             @yield('blueprint.wrappers')
         @show
         @section('scripts')
